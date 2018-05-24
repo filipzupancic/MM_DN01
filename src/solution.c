d19c78c268bd4c30cdcead422afdadfd4bcd53ea
amadejtratnik <at2780@student.uni-lj.si>
2018-05-24 21:19:19 +0200

    Test commit, two particles behaviour

--- src/solution.c
+++ src/solution.c
@@ -6,7 +6,7 @@
 #define NUMBER_OF_BODIES 2    
 #define XYZ 3*NUMBER_OF_BODIES
 
-#define dT 0.0001 //time difference
+#define dT 0.01 //time difference
 #define eps 0.000001 //in case of dividing by 0
 
 /* Array  of particles coordinates: 
@@ -22,19 +22,34 @@ double coordinates[XYZ];
         velocities[2] = v(z0)
 */
 double velocities[XYZ];
+/* Array  of particles velocities: 
+*   Ex: velocities[0] = v(x0)
+        velocities[1] = v(y0)
+        velocities[2] = v(z0)
+*/
 double grav_forces[XYZ];
+/* Array  of particles gravitational forces: 
+*   Ex: grav_forces[0] = F(v(x0))
+        grav_forces[1] = F(v(y0))
+        grav_forces[2] = F(v(z0))
+*/
 
 double force_down;
 double force_up;
 int main(){
-    clock_t start_t, end_t, total_t;
-   start_t = clock();
+    for(int i = 0; i < XYZ;i++){
+        velocities[i] = 0;
+        grav_forces[i] = 0;
+        coordinates[i] = 0;
+    }
 
-    /*for(int i = 0; i < XYZ; i+=3){//start velocity of each particle
-        velocities[i] = 1; //velocity in direction x
-        velocities[i+1] = 0; //velocity in direction y
-        velocities[i+2] = 0; //velocity in direction z
-    }*/
+    coordinates[0] = 5;
+    coordinates[1] = 0;
+    coordinates[2] = 0;
+    coordinates[3] = 50;
+    coordinates[4] = 0;
+    coordinates[5] = 0;
+
     velocities[0] = 1;
     velocities[1] = 0;
     velocities[2] = 0;
@@ -42,40 +57,52 @@ int main(){
     velocities[4] = 0;
     velocities[5] = 0;
 
-    coordinates[0] = 0;
-    coordinates[1] = 0;
-    coordinates[2] = 0;
-    coordinates[3] = 5;
-    coordinates[4] = 0;
-    coordinates[5] = 0;
+   clock_t start_t, end_t, total_t;
+   start_t = clock();
+
 
     double time = dT;
-    while(time < 1){
+    while(time  < 30){
      for(int i = 0; i <XYZ; i+=3){
-printf("%d. telo | koordinata X:%10f | hitrostX: %10f | silaX:%10f\n",i/3,coordinates[i], velocities[i], grav_forces[i]);
+        printf("%d. particle | coordinate X:%10f | velocity_X: %10f | force_X:%10f\n",i/3,coordinates[i], velocities[i], grav_forces[i]);
         for(int j = 0; j < XYZ; j+=3){
             if(i != j){
-                
                 //we calculate the sum of all particles on a singular particle in its own direction
+
+                //current force on particle i in direction x
                 force_down = (abs(coordinates[i] - coordinates[j]+eps));
                 force_down = force_down*force_down*force_down;
                 force_up = coordinates[i] - coordinates[j];
-                grav_forces[i]+= force_up/force_down;  //current force on particle i in direction x
+                grav_forces[i]+= force_up/force_down;  
+                //current force on particle i in direction y
 
                 force_down = (abs(coordinates[i+1] - coordinates[j+1]+eps));
                 force_down = force_down*force_down*force_down;
                 force_up = coordinates[i+1] - coordinates[j+1];
-                grav_forces[i+1]+= force_up/force_down; //current force on particle i in direction y
+                grav_forces[i+1]+= force_up/force_down; 
 
+                //current force on particle i in direction z
                 force_down = (abs(coordinates[i+2] - coordinates[j+2]+eps));
                 force_down = force_down*force_down*force_down;
                 force_up = coordinates[i+2] - coordinates[j+2];
-                grav_forces[i+2]+= force_up/force_down; //current force on particle i in direction z
+                grav_forces[i+2]+= force_up/force_down; 
             }
         }
-         
+        // we upgrade our current velocity and coordinate x of particle i
+        grav_forces[i]*=(-1);
         velocities[i]+=(dT*grav_forces[i]);
         coordinates[i]+=(dT*velocities[i]);
+
+        //changing velocity and coordinate y of particle i
+        grav_forces[i+1]*=(-1);
+        velocities[i+1]+=(dT*grav_forces[i+1]);
+        coordinates[i+1]+=(dT*velocities[i+1]);
+
+        //upgrading coordinate z
+        grav_forces[i+2]*=(-1);
+        velocities[i+2]+=(dT*grav_forces[i+2]);
+        coordinates[i+2]+=(dT*velocities[i+2]);
+        
     }
 
         time+=dT;
