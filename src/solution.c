8e5a4b62b6807a091627aadc305d643ef27e2f44
amadejtratnik <at2780@student.uni-lj.si>
2018-06-04 16:20:58 +0200

    Amlost everything changed

--- src/solution.c
+++ src/solution.c
@@ -5,65 +5,73 @@
 #include "helper.h"
 
 #define dT 10 //time difference
-#define eps 0.000001 //in case of dividing by 0
-double grav_forces[XYZ];
-double M[NUMBER_OF_BODIES];
-double a[XYZ];
-
-
-double G = 0.000667;
+double F[XYZ];
 
 
 void compute_coordinates()
 {
-        double dx = coordinates[0] -coordinates[3];
-        double dy = coordinates[1] - coordinates[4];
-        double D = sqrt(dx*dx + dy*dy);
-        // if(D == 0){
-        //     printf("COLLISION!!\n");
-        //     break;
-        // }
-        double angle_radians = atan2(dy,dx);
+        for(int i = 0; i <  XYZ; i+=3){F[i] = 0;}
+        double dx,dy,dz,D,F1,F2,F3;
 
-        double F = (G*M[0])/ (D*D);
-        a[3] = F*cos(angle_radians);
-        a[4] = F*sin(angle_radians);
-        velocities[3]+=(a[3] *dT);
-        velocities[3]+=(a[4]*dT);
-        coordinates[3]+=(velocities[3]*dT);
-        coordinates[4]+=(velocities[4]*dT);
-    //printf("coordinates[1]: %f coordinates[2]: %f coordinates[3]: %f\n", coordinates[0], coordinates[1], coordinates[2]);
-    //printf("coordinates[4]: %f coordinates[5]: %f coordinates[6]: %f\n", coordinates[3], coordinates[4], coordinates[5]);
+        for(int i = 0; i < XYZ; i+=3){
+            for(int j = 0; j < XYZ; j+=3){
+                if(i != j){
+                 dx = coordinates[i] -coordinates[j];
+                 dy = coordinates[i+1]- coordinates[j+1];
+                 dz = coordinates[i+2] - coordinates[j+2];
+                 D = sqrt(dx*dx + dy*dy + dz*dz);
+                D = pow(D,3);
+                 F1 = (dx/D)*masses[i]*masses[j];
+                 F2 = (dy/D)*masses[i]*masses[j];
+                 F3 =  (dz/D)*masses[i]*masses[j];
+
+                F[i]-=F1;
+                F[i+1]-=F2;
+                F[i+2]-=F3;
+                }
+            }
+        }
+        for(int i = 0; i < XYZ; i+=3){
+            coordinates[i]+=(velocities[i]*dT);
+            coordinates[i+1]+=(velocities[i+1]*dT);
+            coordinates[i+2]+=(velocities[i+2]*dT);
+            
+            velocities[i]+=((F[i]/masses[i])*dT); 
+            velocities[i+1]+=((F[i+1]/masses[i])*dT);
+            velocities[i+2]+=((F[i+2]/masses[i])*dT);
+        }
+
+       
+       
+
+     
 }
 
 int main(int argc, char *argv[])
 {
-    for(int i = 0; i < XYZ;i++)
-    {
-        velocities[i] = 0;
-        grav_forces[i] = 0;
-        coordinates[i] = 0;
-        a[i] = 0;
-    }
+    // for(int i = 0; i < XYZ;i++)
+    // {
+    //     velocities[i] = 0;
+    //     coordinates[i] = 0;
+    // }
+    // coordinates[0] = 320;
+    // coordinates[1] = 240;
+    // coordinates[2] = 0;
 
-    coordinates[0] = 320;
-    coordinates[1] = 240;
-    coordinates[2] = 0;
+    // coordinates[3] = 170;
+    // coordinates[4] = 240;
+    // coordinates[5] = 0;
 
-    coordinates[3] = 170;
-    coordinates[4] = 240;
-    coordinates[5] = 0;
-
-    M[0] = 1098900;
-    M[1] = 0.5974;
+    // M[0] = 1098900;
+    // M[1] = 0.5974;
 
 
-    velocities[0] = 0;
-    velocities[1] = 0;
-    velocities[2] = 0;
-    velocities[3] = 0;
-    velocities[4] = 2;
-    velocities[5] = 0;
+    // velocities[0] = 0;
+    // velocities[1] = 0;
+    // velocities[2] = 0;
+    // velocities[3] = 0;
+    // velocities[4] = 2;
+    // velocities[5] = 0;
 
     visualize(argc, argv);
 
