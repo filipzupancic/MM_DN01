1440475e7b5f22384cddca418ded076607bf05b1
amadejtratnik <at2780@student.uni-lj.si>
2018-06-04 14:10:15 +0200

    planetary motion in C

--- src/solution.c
+++ src/solution.c
@@ -5,121 +5,40 @@
 #include<time.h>
 #include "helper.h"
 
-#define dT 0.01 //time difference
+#define dT 10 //time difference
 #define eps 0.000001 //in case of dividing by 0
-
-
-/* Array  of particles coordinates: 
-*   Ex: coordinates[0] = x0
-        coordinates[1] = y0
-        coordinates[2] = z0
-*/
-//double coordinates[XYZ];
-
-/* Array  of particles velocities: 
-*   Ex: velocities[0] = v(x0)
-        velocities[1] = v(y0)
-        velocities[2] = v(z0)
-*/
-//double velocities[XYZ];
-
-/* Array  of particles gravitational forces: 
-*   Ex: grav_forces[0] = F((x0))
-        grav_forces[1] = F((y0))
-        grav_forces[2] = F((z0))
-*/
 double grav_forces[XYZ];
+double M[NUMBER_OF_BODIES];
+double a[XYZ];
 
 
-double force_down;
-double force_up;
+double G = 0.000667;
 
 
 void compute_coordinates()
 {
-   clock_t start_t, end_t, total_t;
-   start_t = clock();
-
-
-    double time = dT;
-    while(time  < 30){
-
-    double t = 0;
-    while(t  < 150){
-
-     for(int i = 0; i <XYZ; i+=3){
-        //printf("%d. particle | coordinate X:%10f | velocity_X: %10f | force_X:%10f\n",i/3,coordinates[i], velocities[i], grav_forces[i]);
-        for(int j = 0; j < XYZ; j+=3){
-            if(i != j){
-                //we calculate the sum of all particles on a singular particle in its own direction
-                if(coordinates[i] < coordinates[j]){
-                             //current force on particle i in direction x
-                        force_down = (abs(coordinates[i] - coordinates[j]));
-                        force_down = force_down*force_down*force_down;
-                        force_up = coordinates[i] - coordinates[j];
-                        grav_forces[i]+= force_up/(force_down+eps);
-                        //current force on particle i in direction y
-
-                        force_down = (abs(coordinates[i+1] - coordinates[j+1]));
-                        force_down = force_down*force_down*force_down;
-                        force_up = coordinates[i+1] - coordinates[j+1];
-                        grav_forces[i+1]+= force_up/(force_down+eps); 
-
-                        //current force on particle i in direction z
-                        force_down = (abs(coordinates[i+2] - coordinates[j+2]));
-                        force_down = force_down*force_down*force_down;
-                        force_down+=eps;
-                        force_up = coordinates[i+2] - coordinates[j+2];
-                        grav_forces[i+2]+= force_up/(force_down +eps); 
-                }else{
-                             //current force on particle i in direction x
-                        force_down = (abs(coordinates[i] - coordinates[j]));
-                        force_down = force_down*force_down*force_down;
-                        force_up = coordinates[i] - coordinates[j];
-                        grav_forces[i]-= force_up/(force_down+eps);
-                        //current force on particle i in direction y
-
-                        force_down = (abs(coordinates[i+1] - coordinates[j+1]));
-                        force_down = force_down*force_down*force_down;
-                        force_up = coordinates[i+1] - coordinates[j+1];
-                        grav_forces[i+1] -= force_up/(force_down+eps); 
-
-                        //current force on particle i in direction z
-                        force_down = (abs(coordinates[i+2] - coordinates[j+2]));
-                        force_down = force_down*force_down*force_down;
-                        force_down+=eps;
-                        force_up = coordinates[i+2] - coordinates[j+2];
-                        grav_forces[i+2]-= force_up/(force_down +eps); 
-                    
-                }
-                    
-            }
+    
+    while(1){
+        double dx = coordinates[0] -coordinates[3];
+        double dy = coordinates[1] - coordinates[4];
+        double D = sqrt(dx*dx + dy*dy);
+        if(D == 0){
+            printf("COLLISION!!\n");
+            break;
         }
-        // we upgrade our current velocity and coordinate x of particle i
-        //    grav_forces[i]*=(-1);
-        velocities[i]+=(dT*grav_forces[i]);
-        coordinates[i]+=(dT*velocities[i]);
+        double angle_radians = atan2(dy,dx);
 
-        //changing velocity and coordinate y of particle i
-          //  grav_forces[i+1]*=(-1);
-        velocities[i+1]+=(dT*grav_forces[i+1]);
-        coordinates[i+1]+=(dT*velocities[i+1]);
-
-        //upgrading coordinate z
-         //grav_forces[i+2]*=(-1);
-        velocities[i+2]+=(dT*grav_forces[i+2]);
-        coordinates[i+2]+=(dT*velocities[i+2]);
-        
-    }
-        t+=dT;
+        double F = (G*M[0])/ (D*D);
+        a[3] = F*cos(angle_radians);
+        a[4] = F*sin(angle_radians);
+        velocities[3]+=(a[3] *dT);
+        velocities[3]+=(a[4]*dT);
+        coordinates[3]+=(velocities[3]*dT);
+        coordinates[4]+=(velocities[4]*dT);
     }
 
-    printf("coordinates[1]: %f coordinates[2]: %f coordinates[3]: %f\n", coordinates[0], coordinates[1], coordinates[2]);
-    printf("coordinates[4]: %f coordinates[5]: %f coordinates[6]: %f\n", coordinates[3], coordinates[4], coordinates[5]);
-
-    end_t = clock();
-        total_t = (double)(end_t - start_t);
-            printf("Total time taken: %ld miliseconds\n", total_t);
+    //printf("coordinates[1]: %f coordinates[2]: %f coordinates[3]: %f\n", coordinates[0], coordinates[1], coordinates[2]);
+    //printf("coordinates[4]: %f coordinates[5]: %f coordinates[6]: %f\n", coordinates[3], coordinates[4], coordinates[5]);
 }
 
 int main(int argc, char *argv[])
@@ -129,20 +48,25 @@ int main(int argc, char *argv[])
         velocities[i] = 0;
         grav_forces[i] = 0;
         coordinates[i] = 0;
+        a[i] = 0;
     }
 
-    coordinates[0] = 5;
-    coordinates[1] = 0;
+    coordinates[0] = 320;
+    coordinates[1] = 240;
     coordinates[2] = 0;
-    coordinates[3] = 50;
-    coordinates[4] = 0;
+    coordinates[3] = 170;
+    coordinates[4] = 240;
     coordinates[5] = 0;
 
-    velocities[0] = 1;
+    M[0] = 1098900;
+    M[1] = 0.5974;
+
+
+    velocities[0] = 0;
     velocities[1] = 0;
     velocities[2] = 0;
     velocities[3] = 0;
-    velocities[4] = 0;
+    velocities[4] = 2;
     velocities[5] = 0;
 
     visualize(argc, argv);
